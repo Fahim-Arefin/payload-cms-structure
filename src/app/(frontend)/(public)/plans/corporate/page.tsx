bd8f5e303a81d5c81bef88998301f725f82b060b
naimulabeer <naimul.h.abeer@gmail.com>
2025-07-10 00:00:47 +0600

    corporate highlights section modified

--- src/app/(frontend)/(public)/plans/corporate/page.tsx
+++ src/app/(frontend)/(public)/plans/corporate/page.tsx
@@ -4,6 +4,8 @@ import CorporateSuitability from '@/components/custom/corporate/CorporateSuitabi
 import PartnerCarousel from '@/components/custom/corporate/PartnerCarousel'
 import ContactUsSection from '@/components/custom/shared/contactUs/ContactUsSection'
 import HeroSection from '@/components/custom/shared/hero/HeroSection'
+import ToolTip from '@/components/custom/shared/ToolTip'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 type Props = {}
@@ -29,18 +31,19 @@ function page({}: Props) {
     {
       icon: '/assets/corporatebenefits1.png',
       text: 'Comprehensive Coverage',
-      description: 'Protection against natural and accidental deaths, disabilities, and critical illnesses.',
+      description:
+        'Protection against natural and accidental deaths, disabilities, and critical illnesses.',
     },
     {
       icon: '/assets/corporatebenefits2.png',
       text: 'HEALTH BENEFITS',
-      description: 'In-patient, outpatient, maternity, dental, and optical care for total well-being.'
+      description:
+        'In-patient, outpatient, maternity, dental, and optical care for total well-being.',
     },
     {
       icon: '/assets/corporatebenefits3.png',
       text: 'EFFORTLESS CLAIMS',
       description: 'Cashless hospitalization and a smooth claims process.',
-
     },
     {
       icon: '/assets/corporatebenefits4.png',
@@ -54,48 +57,88 @@ function page({}: Props) {
     },
   ]
 
-   const suitabilityData = [
-  {
-    img: "/assets/suitability1.png",
-    title: "FOR LIFE COVERAGE",
-    description:
-      "All regular and healthy eligible employees aged 18 to 64 are qualified for coverage. New hires shall get immediate protection under the scheme from day one seamless and stress-free security from the beginning of their journey with your company.",
-  },
-  {
-    img: "/assets/suitability2.png",
-    title: "FOR LIFE COVERAGE",
-    description:
-      "All eligible employees are suitable for this coverage. Expanding this coverage to employees’ dependents provides an additional level of support. It reflects a deep commitment not only to the employee but also to their family, promoting a sense of security, care and well-being for their entire household.",
-  },
-];
+  const suitabilityData = [
+    {
+      img: '/assets/suitability1.png',
+      title: 'FOR LIFE COVERAGE',
+      description:
+        'All regular and healthy eligible employees aged 18 to 64 are qualified for coverage. New hires shall get immediate protection under the scheme from day one seamless and stress-free security from the beginning of their journey with your company.',
+    },
+    {
+      img: '/assets/suitability2.png',
+      title: 'FOR LIFE COVERAGE',
+      description:
+        'All eligible employees are suitable for this coverage. Expanding this coverage to employees’ dependents provides an additional level of support. It reflects a deep commitment not only to the employee but also to their family, promoting a sense of security, care and well-being for their entire household.',
+    },
+  ]
 
-const partnerData = [
-  {
-    img: '/assets/partner1.png',
-    title: 'GOLDEN HARVEST',
-  },
-  {
-    img: '/assets/partner2.png',
-    title: 'PARTEX STAR GROUP',
-  },
-  {
-    img: '/assets/partner3.png',
-    title: 'AMERICAN & EFRID, INC',
-  },
-  {
-    img: '/assets/partner4.png',
-    title: 'GE HEALTHCARE',
-  },
- 
-]
+  const partnerData = [
+    {
+      img: '/assets/partner1.png',
+      title: 'GOLDEN HARVEST',
+    },
+    {
+      img: '/assets/partner2.png',
+      title: 'PARTEX STAR GROUP',
+    },
+    {
+      img: '/assets/partner3.png',
+      title: 'AMERICAN & EFRID, INC',
+    },
+    {
+      img: '/assets/partner4.png',
+      title: 'GE HEALTHCARE',
+    },
+  ]
 
   return (
     <div className="font-avenir bg-white">
-      <HeroSection heroSlides={heroSlides} />
+      <HeroSection heroSlides={heroSlides}>
+        <div
+          className="absolute top-[245px] md:top-[355px] lg:top-[470px] xl:top-[490px]  2xl:top-[730px] 
+          inset-x-0 -left-[24px] lg:left-[105px] xl:left-[185px] 2xl:left-[258px] lg:right-auto 
+       hero-content-width
+        flex justify-left space-x-4 md:space-x-6 lg:justify-start cursor-not-allowed
+        "
+        >
+          <ToolTip>
+            <Button
+              variant="primary"
+              className="
+            cursor-not-allowed
+            px-2 md:px-6 2xl:px-10
+            py-1 md:py-2 2xl:py-6
+            h-[35px] md:h-[40px] lg:h-[45px] xl:h-[55px] 2xl:h-[60px] 
+            rounded-[4px] lg:rounded-[8px] 
+            w-[100px] md:w-[150px] lg:w-[208.41px] 2xl:w-[258.41px] 
+            global-h4 font-normal"
+            >
+              Explore Now
+            </Button>
+          </ToolTip>
+
+          <ToolTip className="flex items-center space-x-2 text-white 2xl:space-x-4">
+            {/* <div className="flex items-center space-x-2 text-white 2xl:space-x-4"> */}
+            <Button
+              className=" cursor-not-allowed
+            px-2 md:px-6 2xl:px-10
+            py-1 md:py-2 2xl:py-6
+            h-[35px] md:h-[40px] lg:h-[45px] xl:h-[55px] 2xl:h-[60px] 
+            rounded-[4px] lg:rounded-[8px] 
+            w-[100px] md:w-[150px] lg:w-[208.41px] 2xl:w-[258.41px] 
+            global-h4 font-normal border border-white text-white bg-white/20 backdrop-blur-md
+                        hover:bg-white/30 hover:border-white transition-colors duration-300"
+            >
+              Call Now
+            </Button>
+            {/* </div> */}
+          </ToolTip>
+        </div>
+      </HeroSection>
       <CorporateHighlight highlightsData={highlightData} />
-      <CorporateChoose benefitsData={benefitsData}/>
-      <CorporateSuitability data={suitabilityData}/>
-      <PartnerCarousel data={partnerData}/>
+      <CorporateChoose benefitsData={benefitsData} />
+      <CorporateSuitability data={suitabilityData} />
+      <PartnerCarousel data={partnerData} />
       <ContactUsSection />
     </div>
   )
--- src/components/custom/corporate/CorporateHighlights.tsx
+++ src/components/custom/corporate/CorporateHighlights.tsx
@@ -2,33 +2,55 @@ import React, { FC } from 'react'
 
 type Highlights = {
   mainDescription: string
-
 }
 
 type CorporateHighlightProps = {
   highlightsData: Highlights[]
 }
 
-const CorporateHighlight: FC<CorporateHighlightProps> = ({ highlightsData }: CorporateHighlightProps) => {
+const CorporateHighlight: FC<CorporateHighlightProps> = ({
+  highlightsData,
+}: CorporateHighlightProps) => {
   return (
-    
-
-      <div className="relative z-10 container-padding bg-white">
-        <img
-          src="/assets/leftquote.png"
-          alt="Opening Quote"
-          className="hidden lg:block w-12 2xl:w-20 absolute right-20 2xl:right-48 2xl:top-28"
-        />
-        <img
-          src="/assets/rightquote.png" 
-          alt="Closing Quote"
-          className="hidden lg:block w-12 2xl:w-20 absolute left-20 2xl:left-48 2xl:top-28"
-        />
-        <p className="global-h3 text-gray-800 text-center">{highlightsData[0]?.mainDescription}</p>
+    <section className="relative bg-[#8A4F4F] container-padding text-white overflow-hidden">
+      {/* Background Text */}
+      <div className="absolute inset-0 xl:bottom-20 flex justify-center items-center pointer-events-none">
+        <h1 className="text-[4.5rem] md:text-[5.5rem] lg:text-[8rem] font-bold opacity-10 text-center lg:leading-relaxed tracking-wide uppercase whitespace-pre-line">
+          SHANTA LIFE{'\n'}INSURANCE PLC
+        </h1>
       </div>
 
-    
+      {/* Foreground Content */}
+      <div className="">
+        <p className="global-h3 text-white text-left">{highlightsData[0]?.mainDescription}</p>
+
+        <hr className="border-t border-white/50 w-full my-6" />
+
+        {/* Stats Section */}
+        <div>
+          <h2 className="global-h2 font-bold text-center md:text-left uppercase mb-6 lg:mb-10">
+            At a Glance
+          </h2>
+          <div className="flex justify-between text-center px-10">
+            <div className='text-start'>
+              <p className="text-[54px] font-bold">100+</p>
+              <p className="mt-2 text-[1rem]  lg:text-[1.5rem]">Claims</p>
+            </div>
+            <div className='text-start'>
+              <p className="text-[60px] font-bold">
+                3<span className="global-h3 font-semibold">Days</span>
+              </p>
+              <p className="mt-2 text-[1rem] lg:text-[1.5rem]">Settlement</p>
+            </div>
+            <div className='text-start'>
+              <p className="text-[60px] font-bold">92%</p>
+              <p className="mt-2 text-[1rem]  lg:text-[1.5rem]">Success ratio</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default CorporateHighlight;
+export default CorporateHighlight
