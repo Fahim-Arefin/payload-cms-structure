4ef19be1d936c589f5ac7b4686040159901d2409
Fahim-Arefin <fahimarefin57@gmail.com>
2025-07-10 06:20:22 +0600

    Hero section btn adjustment

--- src/app/(frontend)/(public)/plans/individual/saving-and-investment/endowment/page.tsx
+++ src/app/(frontend)/(public)/plans/individual/saving-and-investment/endowment/page.tsx
@@ -2,17 +2,18 @@ import ContactUsSection from '@/components/custom/shared/contactUs/ContactUsSect
 import HeroSection from '@/components/custom/shared/hero/HeroSection'
 import PlanInfoSection from '@/components/custom/shared/PlanInfoSection'
 import { Tab } from '@/components/custom/shared/Tab'
+import ToolTip from '@/components/custom/shared/ToolTip'
+import { Button } from '@/components/ui/button'
 
 import React from 'react'
 
 function EndowmentPage() {
   const heroSlides = [
     {
-      title: '',
+      title: 'Shanta Endowment Plans',
       subtitle: '',
-      description:
-        'The right life insurance doesn\u2019t just wait around for the what-ifs—it walks beside you through the what\u2019s next.',
-      image: '/assets/banner5.jpg',
+      description: 'A promise that grows with time and care',
+      image: '/assets/banner7.jpg',
     },
   ]
 
@@ -130,7 +131,47 @@ function EndowmentPage() {
 
   return (
     <div className="font-avenir bg-white">
-      <HeroSection heroSlides={heroSlides} />
+      <HeroSection heroSlides={heroSlides}>
+        <div
+          className="absolute bottom-12 lg:top-[500px] xl:top-[490px]  2xl:top-[730px] 
+          inset-x-0 -left-[24px] lg:left-[105px] xl:left-[185px] 2xl:left-[258px] lg:right-auto 
+       hero-content-width
+        flex justify-left space-x-4 md:space-x-6 lg:justify-start
+        "
+        >
+          <ToolTip>
+            <Button
+              variant="primary"
+              className="
+            cursor-not-allowed
+            px-2 md:px-6 2xl:px-10
+            py-1 md:py-2 2xl:py-6
+            h-[30px] md:h-[40px] lg:h-[45px] xl:h-[55px] 2xl:h-[60px] 
+            rounded-[4px] lg:rounded-[8px] 
+            w-[100px] md:w-[150px] lg:w-[208.41px] 2xl:w-[258.41px] 
+            global-h4 font-normal"
+            >
+              Purchase Now
+            </Button>
+          </ToolTip>
+          <ToolTip>
+            <Button
+              className=" 
+            cursor-not-allowed
+            px-2 md:px-6 2xl:px-10
+            py-1 md:py-2 2xl:py-6
+            h-[30px] md:h-[40px] lg:h-[45px] xl:h-[55px] 2xl:h-[60px] 
+            rounded-[4px] lg:rounded-[8px] 
+            w-[100px] md:w-[150px] lg:w-[208.41px] 2xl:w-[258.41px] 
+            global-h4 font-normal
+            border border-white text-white bg-white/20 backdrop-blur-md
+             hover:bg-white/30 hover:border-white transition-colors duration-300"
+            >
+              Call Now
+            </Button>
+          </ToolTip>
+        </div>
+      </HeroSection>
       <PlanInfoSection bgColor="#F6EDDD" data={planInfoData} />
       <Tab config={tabItems} data={tabContent} />
       <ContactUsSection />
--- src/app/(frontend)/(public)/plans/page.tsx
+++ src/app/(frontend)/(public)/plans/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from '@/components/custom/shared/hero/HeroSection'
 import AllPlanSection from '@/components/custom/shared/plans/AllPlanSection'
+import ToolTip from '@/components/custom/shared/ToolTip'
 import { Button } from '@/components/ui/button'
 
 function PlanPage() {
@@ -45,25 +46,37 @@ function PlanPage() {
         flex justify-left space-x-4 md:space-x-6 lg:justify-start
         "
         >
-          <Button
-            variant="primary"
-            className="
+          <ToolTip>
+            <Button
+              variant="primary"
+              className="
+            cursor-not-allowed
             px-2 md:px-6 2xl:px-10
-            py-1 md:py-2 2xl:py-6 
+            py-1 md:py-2 2xl:py-6
+            h-[30px] md:h-[40px] lg:h-[45px] xl:h-[55px] 2xl:h-[60px] 
             rounded-[4px] lg:rounded-[8px] 
             w-[100px] md:w-[150px] lg:w-[208.41px] 2xl:w-[258.41px] 
             global-h4 font-normal"
-          >
-            Explore Now
-          </Button>
-
-          <Button
-            className=" px-2 md:px-6 2xl:px-10
-            py-1 md:py-2 2xl:py-6 w-[100px] md:w-[150px] lg:w-[208.41px] 2xl:w-[258.41px] rounded border border-white text-white bg-white/20 backdrop-blur-md
+            >
+              Explore Now
+            </Button>
+          </ToolTip>
+          <ToolTip>
+            <Button
+              className=" 
+            cursor-not-allowed
+            px-2 md:px-6 2xl:px-10
+            py-1 md:py-2 2xl:py-6
+            h-[30px] md:h-[40px] lg:h-[45px] xl:h-[55px] 2xl:h-[60px] 
+            rounded-[4px] lg:rounded-[8px] 
+            w-[100px] md:w-[150px] lg:w-[208.41px] 2xl:w-[258.41px] 
+            global-h4 font-normal
+            border border-white text-white bg-white/20 backdrop-blur-md
              hover:bg-white/30 hover:border-white transition-colors duration-300"
-          >
-            Call Now
-          </Button>
+            >
+              Call Now
+            </Button>
+          </ToolTip>
         </div>
       </HeroSection>
       <AllPlanSection plantData={allPlantData} blur>
