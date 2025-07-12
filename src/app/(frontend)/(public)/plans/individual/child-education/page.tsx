0c8543ba549dc1b375a59112ccee65b994079b31
naimulabeer <naimul.h.abeer@gmail.com>
2025-07-13 02:14:41 +0600

    child and education card section added

--- src/app/(frontend)/(public)/plans/individual/child-education/page.tsx
+++ src/app/(frontend)/(public)/plans/individual/child-education/page.tsx
@@ -39,87 +39,42 @@ const page: FC<pageProps> = ({}) => {
     {
       content: [
         {
+          title: 'Customizable Coverage',
+          description: 'Choose a sum assured based on your child’s future needs.',
+          image: '/assets/childTabIcon1.png',
+        },
+        {
           title: 'Maturity Benefit',
-          description: 'Receive a guaranteed sum assured at the end of the policy term.',
-          image: '/assets/tabimage1.png',
+          description:
+            'Receive the full sum assured at the end of the policy term to support higher education goals.',
+          image: '/assets/childTabIcon2.png',
         },
         {
           title: 'Tax Benefits',
-          description: 'Enjoy tax rebates on premiums, subject to prevailing tax laws.',
-          image: '/assets/tabimage2.png',
+          description: 'Enjoy tax rebates on premiums, avail upto 15% tax rebate. ',
+          image: '/assets/childTabIcon3.png',
         },
-        {
-          title: 'Flexible Premium Payments',
-          description:
-            'Pay your premiums monthly, quarterly, half-yearly, or annually—your choice, your pace.',
-          image: '/assets/tabimage3.png',
-        },
-        {
-          title: 'Life Coverage',
-          description:
-            'In the event of the policyholder’s death during the term, the nominee receives the full sum assured.',
-          image: '/assets/tabimage4.png',
-        },
-        {
-          title: 'Customizable Coverage',
-          description: 'Choose a sum assured that fits your financial goals.',
-          image: '/assets/tabimage5.png',
-        },
+
         {
           title: 'Partner Discounts',
           description:
-            'Get exclusive discounts on medical and diagnostic services at select hospitals and diagnostic centers in our partner network.',
-          image: '/assets/tabimage6.png',
-        },
-      ],
-    },
-    {
-      content: [
-        {
-          title: 'Minimum Entry Age',
-          description: 'Individuals must be at least 18 years old to purchase this plan.',
-          image: '/assets/tabimage1.png',
+            'Avail exclusive discounts on medical and diagnostic services at partnered hospitals and diagnostic centers.',
+          image: '/assets/childTabIcon4.png',
         },
         {
-          title: 'Maximum Entry Age',
-          description: 'Applicants can be up to 60 years old at the time of policy inception.',
-          image: '/assets/tabimage2.png',
+          title: 'Life Coverage',
+          description: 'In the event of the parent’s death, the plan ensures:',
+          listItems: [
+            'Monthly stipend (1%, 2%, or 3% of the sum assured, based on plan choice) paid to the legal guardian till policy maturity.',
+            'Waiver of all future premiums while continuing full coverage.',
+            'Full maturity benefit paid at term-end.',
+          ],
+          image: '/assets/childTabIcon5.png',
         },
         {
-          title: 'Medical Requirements',
-          description: 'Medical underwriting may be required based on sum assured and age.',
-          image: '/assets/tabimage3.png',
-        },
-        {
-          title: 'Valid ID Proof',
-          description: 'A government-issued ID is mandatory for verification.',
-          image: '/assets/tabimage4.png',
-        },
-      ],
-    },
-    {
-      content: [
-        {
-          title: 'Loyalty Additions',
-          description: 'Enjoy loyalty bonuses for staying invested through the entire policy term.',
-          image: '/assets/tabimage1.png',
-        },
-        {
-          title: 'Accidental Death Benefit',
-          description:
-            'Receive an additional payout in case of accidental death, over and above the base sum assured.',
-          image: '/assets/tabimage2.png',
-        },
-        {
-          title: 'Terminal Illness Benefit',
-          description: 'Get early access to the sum assured upon diagnosis of a terminal illness.',
-          image: '/assets/tabimage3.png',
-        },
-        {
-          title: 'Premium Waiver',
-          description:
-            'Future premiums are waived in case of disability or critical illness, as per policy terms.',
-          image: '/assets/tabimage4.png',
+          title: 'Flexible Premium Payments',
+          description: 'Opt for monthly, quarterly, half-yearly, or annual premium modes.',
+          image: '/assets/childTabIcon6.png',
         },
       ],
     },
@@ -128,7 +83,7 @@ const page: FC<pageProps> = ({}) => {
   return (
     <div className="font-avenir bg-white">
       <HeroSection heroSlides={heroSlides} />
-      <ChildEducationPlan data={childInfoData} />
+      <ChildEducationPlan bgColor="#FCF4EB" data={childInfoData} />
       <ChildEducationTabs data={tabContent} config={tabItems} />
     </div>
   )
