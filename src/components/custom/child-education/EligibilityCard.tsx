f1b98e00c5c05e3baec48e51155b7b7429daa826
naimulabeer <naimul.h.abeer@gmail.com>
2025-07-13 16:59:52 +0600

    fixed the child-education page mobile version

--- src/components/custom/child-education/EligibilityCard.tsx
+++ src/components/custom/child-education/EligibilityCard.tsx
@@ -29,7 +29,7 @@ export const EligibilityCard: React.FC<EligibilityCardProps> = ({
 }) => {
   return (
     <div
-      className="rounded-xl p-4 lg:p-12 overflow-hidden shadow-lg bg-cover bg-center flex flex-col"
+      className="rounded-xl p-4 lg:p-12 xl:h-[535px] xl:w-[347px] overflow-hidden shadow-lg bg-cover bg-center flex flex-col"
       style={{
         backgroundImage: `linear-gradient(0deg, rgba(0,0,0,0.40), rgba(0,0,0,0.30)), url('${bgImage}')`,
       }}
@@ -38,10 +38,10 @@ export const EligibilityCard: React.FC<EligibilityCardProps> = ({
         <img src={icon} alt={title} className="w-14 h-14" />
         <p className="uppercase text-white font-bold global-p1 mt-2">{title}</p>
       </div>
-      <div className="flex flex-col justify-center items-center space-y-6 mt-6">
+      <div className="flex flex-col justify-center items-center space-y-2 mt-4">
         {/* Entry Age */}
         <div
-          className="p-4 w-64 flex flex-col items-center shadow-md"
+          className="p-2 w-64 flex flex-col items-center shadow-md"
           style={{
             borderRadius: '6.667px 6.667px 0px 0px',
             background: 'rgba(156, 134, 57, 0.10)',
@@ -51,15 +51,15 @@ export const EligibilityCard: React.FC<EligibilityCardProps> = ({
           <div className="text-white global-p2 uppercase mb-1 font-light">Entry Age</div>
           <div className="flex justify-between w-full px-2 text-white">
             <div className="flex flex-col items-center">
-              <span className="text-xs mb-1 font-light text-[#FCF4EB]">Minimum</span>
-              <span className="global-span font-bold">{entryMin}</span>
-              <span className="text-base font-light">{entryMinLabel}</span>
+              <p className="text-xs font-light text-[#FCF4EB]">Minimum</p>
+              <p className="global-span font-bold">{entryMin}</p>
+              <p className="text-base -mt-2 font-light">{entryMinLabel}</p>
             </div>
 
             <div className="flex flex-col items-center">
-              <span className="text-xs mb-1 font-light">Maximum</span>
-              <span className="global-span font-bold">{entryMax}</span>
-              <span className="text-base font-light">{entryMaxLabel}</span>
+              <p className="text-xs font-light">Maximum</p>
+              <p className="global-span font-bold">{entryMax}</p>
+              <p className="text-base -mt-2 font-light">{entryMaxLabel}</p>
             </div>
           </div>
         </div>
@@ -72,9 +72,7 @@ export const EligibilityCard: React.FC<EligibilityCardProps> = ({
             backdropFilter: 'blur(20px)',
           }}
         >
-          <div className="text-white global-p2 uppercase mb-1 font-light tracking-widest">
-            Policy Term
-          </div>
+          <div className="text-white global-p2 uppercase mb-1 font-light">Policy Term</div>
           <div className="text-white font-bold global-span">
             {policyTerm} <span className="global-span font-light">{policyTermLabel}</span>
           </div>
@@ -88,9 +86,7 @@ export const EligibilityCard: React.FC<EligibilityCardProps> = ({
             backdropFilter: 'blur(20px)',
           }}
         >
-          <div className="text-white global-p2 uppercase mb-1 font-light tracking-widest">
-            Maturity Age
-          </div>
+          <div className="text-white global-p2 uppercase mb-1 font-light">Maturity Age</div>
           <div className="text-white font-bold global-span">
             {maturityAge} <span className="global-span font-light">{maturityAgeLabel}</span>
           </div>
