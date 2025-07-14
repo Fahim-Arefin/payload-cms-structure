f780eff2769622ac701a22b93f3b36b7342b5e44
naimulabeer <naimul.h.abeer@gmail.com>
2025-07-14 23:07:03 +0600

    career page some sections created

--- src/components/custom/career/CareerOpeningPrograms.tsx
+++ src/components/custom/career/CareerOpeningPrograms.tsx
@@ -4,53 +4,55 @@ import React from 'react'
 
 const programOpenings = [
   {
-    img: '/assets/program-internship.png',
+    img: '/assets/Internship.png',
     title: 'INTERNSHIP',
   },
   {
-    img: '/assets/program-trainee.png',
+    img: '/assets/managementTrainee.png',
     title: 'MANAGEMENT TRAINEE',
   },
   {
-    img: '/assets/program-ambassador.png',
+    img: '/assets/campusAmbassador.png',
     title: 'CAMPUS AMBASSADOR',
   },
 ]
 
 function CareerOpeningPrograms() {
   return (
-    <section className="container-padding w-full bg-[#FCF4EB] flex flex-col gap-4">
+    <section className="container-padding w-full bg-[#FCF4EB] flex flex-col gap-4 ">
       {/* HEADER */}
       <div>
         <span className="global-h3 text-[#343434] font-light block mb-0">FIND OUT THE</span>
-        <h2 className="global-h1 font-bold tracking-tight">
+        <h2 className="global-h1 font-bold">
           OPENING <span className="text-[#ED7125]">PROGRAMS</span>
         </h2>
       </div>
       {/* CONTENT */}
       <div
         className="
-        flex flex-col md:flex-row items-center
-        justify-between mt-4 md:mt-10 gap-6 md:gap-10
+        grid grid-cols-[0.8fr_1.2fr] md:grid-cols-2
+        items-center
+        text-center
+        mt-1 md:mt-10 gap-6 md:gap-10 2xl:px-10
       "
       >
         {/* Cards List */}
         <div
           className="
             w-full md:w-auto flex flex-col md:items-start
-            md:justify-center gap-7 z-10
-            order-2 md:order-1
+            md:justify-center gap-2 md:gap-6 lg:gap-10 z-10
+            order-1 md:order-2
           "
         >
           {programOpenings.map((item: any) => (
-            <div key={item.title} className="flex items-center gap-4 md:gap-5">
+            <div key={item.title} className="flex flex-col md:flex-row items-center gap-4 md:gap-5">
               <img
                 src={item.img}
                 alt={item.title}
-                className="w-[52px] h-[52px] md:w-[58px] md:h-[58px] select-none"
+                className="w-[50px] h-[50px] md:w-[100px] md:h-[100px] lg:w-[167px] lg:h-[167px] select-none"
                 draggable={false}
               />
-              <span className="text-[#343434] text-[20px] md:text-[23px] font-semibold tracking-tight">
+              <span className="text-[#343434] text-[12px] md:text-[20px] xl:text-[32px] font-semibold">
                 {item.title}
               </span>
             </div>
@@ -59,18 +61,15 @@ function CareerOpeningPrograms() {
         {/* Banner */}
         <div
           className="
-             flex justify-center md:justify-end
-            w-full md:w-[390px] lg:w-[430px] xl:w-[490px]
-            min-w-[230px] max-w-[360px] md:max-w-none
-            mx-auto md:mx-0
-            relative
+              relative
+              order-2 md:order-1
           "
         >
           <img
             src="/assets/programOpeningsBanner.png"
             alt="Programs Banner"
-            className="
-              lg:w-[547px] lg:h-[500px]
+            className="w-[400px] h-[360px] md:h-[361px]
+              lg:w-[547px] lg:h-[550px]
               object-cover
             "
             draggable={false}
