8c5ae6f55f75ba6c98174300f253ea93678bf856
naimulabeer <naimul.h.abeer@gmail.com>
2025-07-21 06:16:34 +0600

    corporate highlight section title animation fixation

--- src/components/custom/corporate/CorporateHighlights.tsx
+++ src/components/custom/corporate/CorporateHighlights.tsx
@@ -15,8 +15,9 @@ const CorporateHighlight: FC<CorporateHighlightProps> = ({
     <section className="relative bg-[#9A4E46] container-padding text-white overflow-hidden">
       {/* Background Text */}
       <div className="absolute inset-0 xl:bottom-20 flex justify-center items-center pointer-events-none">
-        <h1 className="text-[4.5rem] md:text-[5.5rem] lg:text-[8rem] font-bold opacity-10 text-center lg:leading-relaxed tracking-wide uppercase whitespace-pre-line animate-fadeSlideLeft md:animate-fadeSlideRight">
-          SHANTA LIFE{'\n'}INSURANCE PLC
+        <h1 className="text-[4.5rem] md:text-[5.5rem] lg:text-[8rem] font-bold text-center lg:leading-relaxed tracking-wide uppercase whitespace-pre-line">
+          <span className="block animate-fadeSlideLeft">SHANTA LIFE</span>
+          <span className="block animate-fadeSlideRight">INSURANCE PLC</span>
         </h1>
       </div>
 
