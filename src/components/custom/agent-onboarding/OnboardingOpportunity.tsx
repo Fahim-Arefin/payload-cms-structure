a4a20eff767794e14d40984d0b59be81d6286dee
Fahim-Arefin <fahimarefin57@gmail.com>
2025-07-25 08:11:15 +0600

    41 done

--- src/components/custom/agent-onboarding/OnboardingOpportunity.tsx
+++ src/components/custom/agent-onboarding/OnboardingOpportunity.tsx
@@ -125,7 +125,7 @@ export default function OnboardingOpportunity({ opportunityData, expectedData }:
               alt={expectedData.sectionRight.name}
               className="w-[100px] h-[100px] lg:w-[208px] lg:h-[208px] rounded-full object-cover mb-4"
             />
-            <h4 className="global-p2 font-semibold text-[#9A4E46] mb-2 lg:mb-6 lg:w-[25%]">
+            <h4 className="global-p2 font-semibold text-[#9A4E46] mb-2 lg:mb-6 ">
               {expectedData.sectionRight.name}
             </h4>
             <p className="global-p2 text-[#3A3A3C]">
