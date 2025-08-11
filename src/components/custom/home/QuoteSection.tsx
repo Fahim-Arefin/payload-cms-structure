87e104d2435ce4da3ce9cb48f54606527813df46
Fahim-Arefin <fahimarefin57@gmail.com>
2025-08-11 18:04:23 +0600

    conflict resolved

--- src/components/custom/home/QuoteSection.tsx
+++ src/components/custom/home/QuoteSection.tsx
@@ -98,12 +98,8 @@ function QuoteSection() {
         {/* Left Side - Results Section (order-2 on mobile, order-1 on lg+) */}
         <div
           ref={resultRef}
-<<<<<<< HEAD
           className="lg:p-1 xl:p-2 2xl:p-3
           lg:pl-0 space-y-8 xl:space-y-11 z-10 order-2 lg:order-1"
-=======
-          className="p-4 lg:pl-0 space-y-8 xl:space-y-11 z-10 order-2 lg:order-1"
->>>>>>> 3c9fa3d0d9f37f1b342f1adbc433d01844a6e5db
         >
           {/* Text Container */}
           <div className="hidden lg:block space-y-2 text-center lg:text-left">
@@ -396,11 +392,7 @@ function QuoteSection() {
           )}
           {/* Calculate Again Button - Only show on mobile when results are available */}
           {apiResponse && (
-<<<<<<< HEAD
             <div className="flex justify-center items-center lg:hidden ">
-=======
-            <div className="flex justify-center items-center lg:hidden mt-4">
->>>>>>> 3c9fa3d0d9f37f1b342f1adbc433d01844a6e5db
               <GlobalButton
                 onClick={handleCalculateAgain}
                 className=""
