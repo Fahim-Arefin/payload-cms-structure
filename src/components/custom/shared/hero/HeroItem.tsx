2a0e584e07ade59301542da9edb40129da2a29e8
Fahim-Arefin <fahimarefin57@gmail.com>
2025-08-20 00:47:47 +0600

    home page bug fixed

--- src/components/custom/shared/hero/HeroItem.tsx
+++ src/components/custom/shared/hero/HeroItem.tsx
@@ -19,9 +19,9 @@ const HeroItem = ({ slide, top, position }: Props) => {
         src={slide.image}
         alt={slide.title}
         fill
-        priority
         className={`object-cover ${position}`}
         sizes="(max-width: 1349px) 50vw, 100vw"
+        priority
       />
       {/* Overlay */}
       <div className="absolute inset-0 z-10 bg-black/35" />
