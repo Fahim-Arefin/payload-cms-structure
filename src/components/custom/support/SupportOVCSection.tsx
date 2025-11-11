d7b76363455cbbb8c8f47001124343d67f351e1f
Fahim Arefin <fahimarefin57@gmail.com>
2025-11-11 12:32:28 +0600

    tiny chance

--- src/components/custom/support/SupportOVCSection.tsx
+++ src/components/custom/support/SupportOVCSection.tsx
@@ -157,7 +157,7 @@ const SupportOVCSection: FC<SupportOVCSectionProps> = ({
                   {videoLink && (
                     <iframe
                       className="relative z-10 w-full h-full"
-                      src={videoLink}
+                      src={videoLink || ''}
                       title="YouTube video player"
                       frameBorder={0}
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
