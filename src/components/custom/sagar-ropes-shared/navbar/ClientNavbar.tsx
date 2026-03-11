53267bff03aa382f9ec5e8e6c9693e843a8d5316
Fahim Arefin <fahimarefin57@gmail.com>
2026-03-11 06:35:44 +0600

    Navbar mobile version added

--- src/components/custom/sagar-ropes-shared/navbar/ClientNavbar.tsx
+++ src/components/custom/sagar-ropes-shared/navbar/ClientNavbar.tsx
@@ -62,6 +62,7 @@ import { NavbarData } from './ServerNavbar'
 import Image from 'next/image'
 import Link from 'next/link'
 import Menu from './Menu'
+import MobileNavbar from './MobileNavbar'
 
 type Props = { data: NavbarData; blur: string }
 
@@ -70,49 +71,54 @@ function ClientNavbar({ data, blur }: Props) {
     data.branding.logo?.url ?? `${process?.env?.NEXT_PUBLIC_STATIC_IMG_DOMAIN}/images/logo.png`
 
   return (
-    <div
-      className="fixed inset-x-0 z-50 w-full flex flex-row
+    <>
+      {/* mobile */}
+      <MobileNavbar data={data} blur={blur} />
+
+      <div
+        className="fixed inset-x-0 z-50 w-full hidden lg:flex 
      h-[60px] lg:h-[75px] xl:h-[80px] 2xl:h-[90px]
     border-b-[3px] border-b-dark-3 backdrop-blur-[15px]
     "
-      style={{
-        background:
-          'linear-gradient(0deg, rgba(255,255,255,0.20) 0%, rgba(255,255,255,0.20) 100%), rgba(255,255,255,0.20)',
-      }}
-    >
-      {/* logo */}
-      <div
-        className="flex items-center justify-center
+        style={{
+          background:
+            'linear-gradient(0deg, rgba(255,255,255,0.20) 0%, rgba(255,255,255,0.20) 100%), rgba(255,255,255,0.20)',
+        }}
+      >
+        {/* logo */}
+        <div
+          className="flex items-center justify-center
                 pl-4 lg:pl-6 xl:pl-10 2xl:pl-12
                 w-[200px] lg:w-[220px] xl:w-[300px] 2xl:w-[400px]
        "
-      >
-        <div
-          className="relative aspect-[701/179]
+        >
+          <div
+            className="relative aspect-[701/179]
         hover:scale-110 transition-all duration-300 ease-in
         w-[100px] lg:w-[140px] xl:w-[180px] 2xl:w-[200px] "
-        >
-          <Link href="/" aria-label="Home">
-            {typeof data.branding.logo === 'object' && data.branding.logo?.url && (
-              <Image
-                src={logoUrl}
-                alt="Company logo"
-                fill
-                className="object-contain w-full h-full"
-                priority
-                placeholder="blur"
-                blurDataURL={blur || ''}
-                quality={80}
-              />
-            )}
-          </Link>
+          >
+            <Link href="/" aria-label="Home">
+              {typeof data.branding.logo === 'object' && data.branding.logo?.url && (
+                <Image
+                  src={logoUrl}
+                  alt="Company logo"
+                  fill
+                  className="object-contain w-full h-full"
+                  priority
+                  placeholder="blur"
+                  blurDataURL={blur || ''}
+                  quality={80}
+                />
+              )}
+            </Link>
+          </div>
+        </div>
+        {/* border border-black */}
+        <div className=" grow ">
+          <Menu data={data} />
         </div>
       </div>
-      {/* border border-black */}
-      <div className=" grow ">
-        <Menu data={data} />
-      </div>
-    </div>
+    </>
   )
 }
 
