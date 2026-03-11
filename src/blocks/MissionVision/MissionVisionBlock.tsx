81d1ba7bc64ce13abe849f23c5a25fdb007c0091
Fahim Arefin <fahimarefin57@gmail.com>
2026-03-12 03:45:53 +0600

    missionvision block BG image added

--- src/blocks/MissionVision/MissionVisionBlock.tsx
+++ src/blocks/MissionVision/MissionVisionBlock.tsx
@@ -37,6 +37,9 @@ import IntroSection from '@/components/custom/sagar-ropes-shared/others/IntroSec
 import WithHashScroller from '@/components/custom/sagar-ropes-shared/others/WithHashScroller'
 import { MissionVisionBlockType } from '@/types/payloadCustomTypes'
 import MissionVisionCard from './components/MissionVisionCard'
+import Image from 'next/image'
+import Pattern01 from '/public/assets/images/BOpattern2.png'
+import Pattern02 from '/public/assets/images/BOpattern2.png'
 
 type Props = {
   block: MissionVisionBlockType
@@ -46,9 +49,9 @@ type Props = {
 function MissionVisionBlock({ block }: Props) {
   return (
     <WithHashScroller id={block?.sectionId} bgColor={block?.backgroundColor}>
-      <div>
-        {/* section intro */}
+      <div className="relative z-10">
         <div className="container-padding">
+          {/* section intro */}
           <IntroSection block={block} />
           {/* mission and vision */}
           <div
@@ -75,6 +78,51 @@ function MissionVisionBlock({ block }: Props) {
             />
           </div>
         </div>
+
+        {/* pattern 1 */}
+        {block?.showPatternDesign && (
+          <div
+            className="invisible md:visible absolute left-0 top-0 z-30 h-[50%]
+                   md:w-[120px] 
+                   lg:w-[170px] 
+                   xl:w-[225px] 
+                   2xl:w-[280px]
+                "
+          >
+            <Image
+              fill
+              src={Pattern01}
+              alt="pattern image 02"
+              quality={90}
+              sizes="100vw"
+              className=""
+              placeholder="blur"
+              blurDataURL={Pattern01?.blurDataURL}
+            />
+          </div>
+        )}
+        {/* pattern 2 */}
+        {block?.showPatternDesign && (
+          <div
+            className="invisible md:visible absolute right-0 bottom-0 z-30 h-[50%]
+                   md:w-[120px] 
+                   lg:w-[170px] 
+                   xl:w-[225px] 
+                   2xl:w-[280px]
+                "
+          >
+            <Image
+              fill
+              src={Pattern02}
+              alt="pattern image 02"
+              quality={90}
+              sizes="100vw"
+              className="object-cover rotate-180"
+              placeholder="blur"
+              blurDataURL={Pattern02?.blurDataURL}
+            />
+          </div>
+        )}
       </div>
     </WithHashScroller>
   )
