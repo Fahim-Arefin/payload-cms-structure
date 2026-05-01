c7cfdba0a3032976a01cfab82d5cfdb0d698b709
Fahim Arefin <fahimarefin57@gmail.com>
2026-05-02 02:07:48 +0600

    Info card 04 hover description scale not off

--- src/blocks/ProductInfo04/components/InfoCard04.tsx
+++ src/blocks/ProductInfo04/components/InfoCard04.tsx
@@ -23,30 +23,6 @@ function InfoCard04({ data, index, className, height, padding }: Props) {
     >
       <div className="space-y-2 lg:space-y-3 xl:space-y-4 2xl:space-y-5 ">
         {typeof data?.icon === 'object' && data?.icon?.url && (
-          // <div
-          //   className="border-[1.125px] border-[rgba(16,16,83,0.15)] bg-[linear-gradient(135deg,rgba(16,16,83,0.30)_0%,rgba(16,16,83,0)_50%,rgba(16,16,83,0.30)_100%)]
-          //                   w-[45px] md:w-[55px] lg:w-[65px] xl:w-[75px] 2xl:w-[85px]
-          //                   h-[45px] md:h-[55px] lg:h-[65px] xl:h-[75px] 2xl:h-[85px]
-          //                   rounded-full
-          //                   flex items-center justify-center
-          //                   p-1 xl:p-1.5 2xl:p-2
-          //                   group-hover/testCard:bg-[linear-gradient(135deg,rgba(255,255,255)_0%,rgba(255,255,255)_50%,rgba(255,255,255)_100%)]
-          //                   transition-all duration-300 ease-in
-          //     "
-          // >
-          //   <div className="relative w-full aspect-[5/4] group-hover/testCard:scale-90 transition-all delay-150 duration-300 ease-out">
-          //     <Image
-          //       key={index}
-          //       src={data?.icon?.url}
-          //       alt="icon"
-          //       fill
-          //       sizes="100vw"
-          //       className={`object-cover object-center w-full h-full`}
-          //       placeholder={data?.iconBlurDataURL ? 'blur' : 'empty'}
-          //       blurDataURL={data?.iconBlurDataURL || undefined}
-          //     />
-          //   </div>
-          // </div>
           <div
             className="
     relative overflow-hidden
@@ -97,17 +73,17 @@ function InfoCard04({ data, index, className, height, padding }: Props) {
           <div className="relative">
             {/* initial */}
             <div
-              className={`opacity-100 group-hover/testCard:opacity-0 group-hover/testCard:scale-110 
+              className={`group-hover/testCard:text-white-1
                 font-manrope global-p5 text-dark-3 transition-all duration-300 ease-in text-justify`}
             >
               <LocalizedRichText en={data?.description} bn={data?.description} />
             </div>
-            <div
+            {/* <div
               className={`absolute inset-0 opacity-0  scale-110 group-hover/testCard:opacity-100 group-hover/testCard:scale-100  
                 font-manrope global-p5 text-white-1 transition-all delay-150 duration-300 ease-in text-justify`}
             >
               <LocalizedRichText en={data?.description} bn={data?.description} />
-            </div>
+            </div> */}
           </div>
         )}
       </div>
