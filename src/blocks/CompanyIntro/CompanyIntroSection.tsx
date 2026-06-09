04e80fa5a0921bec7f9c5997508c60c20b709e90
Fahim <fahimarefin57@gmail.com>
2026-06-09 13:29:14 +0600

    company intro section blur effect added

--- src/blocks/CompanyIntro/CompanyIntroSection.tsx
+++ src/blocks/CompanyIntro/CompanyIntroSection.tsx
@@ -2,6 +2,8 @@ import SectionHeading01 from '@/components/custom/sagar-ropes-shared/others/Sect
 import LocalizedRichText from '@/components/custom/shared/LocalizedRichText'
 import { CompanyIntroBlockType } from '@/types/payloadCustomTypes'
 import React from 'react'
+import Eclipse from 'public/assets/images/introDesign.png'
+import Image from 'next/image'
 
 type Props = {
   block: CompanyIntroBlockType
@@ -12,7 +14,7 @@ function CompanyIntroSection({ block }: Props) {
     !!block?.companyIntroDescription?.description &&
     !!block?.companyIntroDescription?.description?.root?.direction // or lexicalHasRealText(block.description?.root)
   return (
-    <div className="container-padding space-y-[20px] lg:space-y-[40px] xl:space-y-[56px] 2xl:space-y-[60px]">
+    <div className="relative container-padding space-y-[20px] lg:space-y-[40px] xl:space-y-[56px] 2xl:space-y-[60px]">
       <SectionHeading01 data={block?.sectionHeading} align="middle" />
       {hasDesc && (
         <div className="font-grift text-justify text-secondary-1 global-p2">
@@ -22,6 +24,18 @@ function CompanyIntroSection({ block }: Props) {
           />
         </div>
       )}
+      <div className="absolute bottom-0 left-0 right-0 h-[80%]">
+        <Image
+          src={Eclipse}
+          alt="Eclipse"
+          sizes="100vw"
+          quality={90}
+          placeholder="blur"
+          fill
+          blurDataURL={Eclipse?.blurDataURL}
+          className="w-full h-full"
+        />
+      </div>
     </div>
   )
 }
